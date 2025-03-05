bb58970b37817ad1234744d2596e5af47b076b21
italorjesus <italorjesus@gmail.com>
2025-03-04 22:11:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
 def calcular_gordura(sexo, idade, abdominal, coxa, antebraco, panturrilha, braco_superior, nadegas):
-if sexo == "Feminino":
-if idade <= 26:
-return (abdominal * 0.5276) + (coxa * 0.8184) - (antebraco * 1.6967) - 19.6
-else:
-return (abdominal * 0.4682) + (coxa * 0.4886) - (panturrilha * 0.5703) - 18.4
-elif sexo == "Masculino":
-if idade <= 26:
-return (braco_superior * 1.4564) + (abdominal * 0.5157) - (antebraco * 2.1382) - 10.2
-else:
-return (nadegas * 0.4147) + (abdominal * 0.3558) - (antebraco * 1.1814) - 15.0
-else:
-return "Erro: Sexo inválido"
+  if sexo == "Feminino":
+    if idade <= 26:
+      return (abdominal * 0.5276) + (coxa * 0.8184) - (antebraco * 1.6967) - 19.6
+    else:
+      return (abdominal * 0.4682) + (coxa * 0.4886) - (panturrilha * 0.5703) - 18.4
+  elif sexo == "Masculino":
+    if idade <= 26:
+      return (braco_superior * 1.4564) + (abdominal * 0.5157) - (antebraco * 2.1382) - 10.2
+    else:
+      return (nadegas * 0.4147) + (abdominal * 0.3558) - (antebraco * 1.1814) - 15.0
+  else:
+    return "Erro: Sexo inválido"
 
 # Interface Streamlit
 st.title("Calculadora de Gordura Corporal")
