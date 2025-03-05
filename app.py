3e1693f3ae1eea0fc8d915efe28f4038590a3844
italorjesus <italorjesus@gmail.com>
2025-03-04 22:14:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
 def calcular_gordura(sexo, idade, abdominal, coxa, antebraco, panturrilha, braco_superior, nadegas):
-  if sexo == "Feminino":
-    if idade <= 26:
-      return (abdominal * 0.5276) + (coxa * 0.8184) - (antebraco * 1.6967) - 19.6
+    if sexo == "Feminino":
+        if idade <= 26:
+            return (abdominal * 0.5276) + (coxa * 0.8184) - (antebraco * 1.6967) - 19.6
+        else:
+            return (abdominal * 0.4682) + (coxa * 0.4886) - (panturrilha * 0.5703) - 18.4
+    elif sexo == "Masculino":
+        if idade <= 26:
+            return (braco_superior * 1.4564) + (abdominal * 0.5157) - (antebraco * 2.1382) - 10.2
+        else:
+            return (nadegas * 0.4147) + (abdominal * 0.3558) - (antebraco * 1.1814) - 15.0
     else:
-      return (abdominal * 0.4682) + (coxa * 0.4886) - (panturrilha * 0.5703) - 18.4
-  elif sexo == "Masculino":
-    if idade <= 26:
-      return (braco_superior * 1.4564) + (abdominal * 0.5157) - (antebraco * 2.1382) - 10.2
-    else:
-      return (nadegas * 0.4147) + (abdominal * 0.3558) - (antebraco * 1.1814) - 15.0
-  else:
-    return "Erro: Sexo inválido"
+        return "Erro: Sexo inválido"
 
 # Interface Streamlit
 st.title("Calculadora de Gordura Corporal")
@@ -25,9 +25,4 @@ coxa = st.number_input("Circunferência da Coxa (cm)", min_value=0.0, format="%.
 antebraco = st.number_input("Circunferência do Antebraço (cm)", min_value=0.0, format="%.2f")
 panturrilha = st.number_input("Circunferência da Panturrilha (cm)", min_value=0.0, format="%.2f")
 braco_superior = st.number_input("Circunferência do Braço Superior (cm)", min_value=0.0, format="%.2f")
-nadegas = st.number_input("Circunferência das Nádegas (cm)", min_value=0.0, format="%.2f")
-
-# Botão para calcular
-  if st.button("Calcular % de Gordura Corporal"):
-    resultado = calcular_gordura(sexo, idade, abdominal, coxa, antebraco, panturrilha, braco_superior, nadegas)
-    st.success(f"Percentual estimado de gordura corporal: {resultado:.2f}%")
+nadegas = st.num
